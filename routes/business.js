252d3ccbddd3cb386d9c8a612fe451ec553d1559
Peter <panxin19911020@gmail.com>
2016-10-27 20:40:15 -0400

    add a business route

---
+++ routes/business.js
@@ -0,0 +1,24 @@
+/**
+ * Created by XIN on 2016/10/27.
+ */
+const express = require('express');
+const router = express.Router();
+//mongoose -> dataBase
+const mongoose = require('mongoose');
+require('../models/yelp');
+mongoose.connect('mongodb://localhost:27017/yelp');
+const Business = mongoose.model('Business');
+
+router.get("/", (req, res) => {
+    Business.find().then((allData) => {
+        res.json(allData);
+
+
+    }, () => {
+        // Something went wrong with the server!
+        res.status(500).send();
+    });
+
+});
+
+module.exports = router;
